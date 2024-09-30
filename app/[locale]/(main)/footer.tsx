09327eac71d0409f3f4341a18c11497ecd2f21de
Yvonne Lu <minmemory@users.noreply.github.com>
2024-10-01 02:01:21 +0800

    Update footer.tsx

--- app/[locale]/(main)/footer.tsx
+++ app/[locale]/(main)/footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           {t("section_607-869_2020")}
           {new Date().getFullYear()}{" "}
-          <a href={"https://buycoffee.top"}>{t("a_800-850_Hamster1963")}</a> Modified by <a href={"https://mm.sd"}>{t("a_800-850_YvonneLu")}</a>
+          <a href={"https://buycoffee.top"}>{t("a_800-850_Hamster1963")}</a> Modified by <a href={"https://mm.sd"}>{t("MinMin")}</a>
         </section>
       </section>
     </footer>
