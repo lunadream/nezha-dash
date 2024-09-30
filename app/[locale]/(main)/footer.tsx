0f7ad3d5c961c4c4744f02c0e235d67dcfb3d65f
Yvonne Lu <minmemory@users.noreply.github.com>
2024-10-01 00:15:23 +0800

    Update footer.tsx
    Modify contact

--- app/[locale]/(main)/footer.tsx
+++ app/[locale]/(main)/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <p className="mt-3 flex gap-1 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           {t("p_146-598_Findthecodeon")}{" "}
           <a
-            href="https://github.com/hamster1963/nezha-dash"
+            href="https://github.com/lunadream/nezha-dash"
             target="_blank"
             className="cursor-pointer font-normal underline decoration-yellow-500 decoration-2 underline-offset-2 dark:decoration-yellow-500/50"
           >
@@ -17,7 +17,7 @@ export default function Footer() {
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           {t("section_607-869_2020")}
           {new Date().getFullYear()}{" "}
-          <a href={"https://buycoffee.top"}>{t("a_800-850_Hamster1963")}</a>
+          <a href={"https://buycoffee.top"}>{t("a_800-850_Hamster1963")}</a> Modified by <a href={"https://mm.sd"}>{t("a_800-850_YvonneLu")}</a>
         </section>
       </section>
     </footer>
